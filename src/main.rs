bfc19b222551d211ee103b8b1761b36668800836
Christopher Moran <christophermoran@ucsb.edu>
2021-03-22 23:07:16 -0700

    Initial support for nav_types crate for position info

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ use std::io::{self, Write};
 use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
+use nav_types::*;
 
 fn main() {
     let port_name = "/dev/ttyACM0";
     let baudrate = 115_200;
     let mut nmea = Nmea::new();
     let (mut lat, mut lon) = (0.0, 0.0);
-    let (tx, rx) = mpsc::channel();
+    let (tx_gps, rx_gps) = mpsc::channel();
 
     let gps = thread::spawn(move || {
         let mut port = serialport::new(port_name, baudrate)
@@ -44,7 +45,7 @@ fn main() {
                                     Some(val) => val,
                                     _ => lon,
                                 };
-                                tx.send((lat, lon)).unwrap();
+                                tx_gps.send((lat, lon)).unwrap();
                             }
                             Err(e) => {
                                 // println!("ERROR PARSING")
@@ -62,7 +63,7 @@ fn main() {
     let eps = 1e-6;
     let (mut lat, mut lon) = (0.0, 0.0);
     loop {
-        match rx.recv() {
+        match rx_gps.recv() {
             Ok((a, b)) => {
                 if ((lat - a) > eps) {
                     lat = a;
@@ -77,9 +78,46 @@ fn main() {
             }
             Err(e) => (),
         };
-        println!("(lat, lon) = ({}, {})", lat, lon);
+        let position = WGS84::from_degrees_and_meters(lat, lon, 0.0);
+        println!("Current position: {:?}",position);
         thread::sleep(Duration::from_millis(2000));
     }
 
     gps.join().unwrap();
+}
+
+// Based on `rust-navigation` crate
+use std::f64::consts::PI;
+fn estimate_bearing(a: WGS84<f64>, b: WGS84<f64>) -> f64 {
+
+    let start_lat = a.latitude_radians();
+    let start_lon = a.longitude_radians();
+    let dest_lat = b.latitude_radians();
+    let dest_lon = b.longitude_radians();
+
+    let mut delta_lon = dest_lon - start_lon;
+
+    let delta_phi =
+    ((dest_lat / 2.0 + PI / 4.0).tan() / (start_lat / 2.0 + PI / 4.0).tan()).ln();
+
+    if delta_lon.abs() > PI {
+        if delta_lon > 0.0 {
+            delta_lon = -(2.0 * PI - delta_lon);
+        } else {
+            delta_lon += 2.0 * PI;
+        }
+    }
+
+    let bearing = (delta_lon.atan2(delta_phi).to_degrees() + 360.0) % 360.0;
+    let bearing = match bearing <= 180.0 {
+        true => {
+            360.0 - bearing
+        },
+        false => {
+            -bearing
+        }
+
+    };
+    bearing
+
 }
