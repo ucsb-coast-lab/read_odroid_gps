31297acfb09f3d3be9fb949ed0f4e4b5034ddfff
Christopher Moran <christophermoran@ucsb.edu>
2021-03-23 17:54:50 -0700

    Adds Pier + Point examples

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use nav_types::*;
 use nmea::Nmea;
-use std::io::{self, Write};
+use std::io;
 use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
@@ -33,7 +33,7 @@ fn main() {
                     for phrase in &phrases {
                         // println!("PHRASE: {}",phrase);
                         match nmea.parse(phrase) {
-                            Ok(parsed) => {
+                            Ok(_) => {
                                 // println!("Successfully parsed to: {:?} ", nmea.latitude);
                                 // println!("(lat, long): ({}, {})",nmea.latitude.unwrap(), nmea.longitude.unwrap());
 
@@ -47,8 +47,8 @@ fn main() {
                                 };
                                 tx_gps.send((lat, lon)).unwrap();
                             }
-                            Err(e) => {
-                                // println!("ERROR PARSING")
+                            Err(_) => {
+                                //println!("{:?}",e)
                                 ()
                             }
                         }
@@ -62,28 +62,46 @@ fn main() {
 
     let eps = 1e-6;
     let (mut lat, mut lon) = (0.0, 0.0);
-    loop {
+
+    for _ in 0..10 {
         match rx_gps.try_recv() {
             Ok((a, b)) => {
-                if ((lat - a) > eps) {
+                if lat - a > eps {
                     lat = a;
                     println!("(lat, lon) = ({}, {})", lat, lon);
                 }
-                if ((lon - b) > eps) {
+                if lon - b > eps {
                     lon = b;
                     println!("(lat, lon) = ({}, {})", lat, lon);
                 }
                 lat = a;
                 lon = b;
             }
-            Err(e) => (),
+            Err(_) => (),
         };
-        let position = WGS84::from_degrees_and_meters(lat, lon, 0.0);
+        let actual = WGS84::from_degrees_and_meters(lat, lon, 0.0);
         println!(
             "Current position: ({},{})",
-            position.latitude_degrees(),
-            position.longitude_degrees()
+            actual.latitude_degrees(),
+            actual.longitude_degrees()
         );
+        let point = WGS84::from_degrees_and_meters(34.404507405298624, -119.84407870487519, 0.0);
+        let pier = WGS84::from_degrees_and_meters(34.41296949371489, -119.82826439753913, 0.0);
+
+        let point_distance = actual.distance(&point);
+        let point_bearing = estimate_bearing(actual, point);
+        println!(
+            "The Point is {:.2} m away at a bearing of appx. {} degrees",
+            point_distance, point_bearing
+        );
+
+        let pier_distance = actual.distance(&pier);
+        let pier_bearing = estimate_bearing(actual, pier);
+        println!(
+            "The Pier is {:.2} m away at a bearing of appx. {} degrees",
+            pier_distance, pier_bearing
+        );
+
         thread::sleep(Duration::from_millis(1000));
     }
 
@@ -112,8 +130,8 @@ fn estimate_bearing(a: WGS84<f64>, b: WGS84<f64>) -> f64 {
 
     let bearing = (delta_lon.atan2(delta_phi).to_degrees() + 360.0) % 360.0;
     let bearing = match bearing <= 180.0 {
-        true => 360.0 - bearing,
-        false => -bearing,
+        true => bearing,
+        false => -(360.0 - bearing),
     };
     bearing
 }
